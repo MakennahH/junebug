fc5c64d080b542b28dc9b7640a5aeee5a59c48c2
MakennahH <kena.henry2@icloud.com>
2020-12-10 15:55:15 -0500

    messing around with writing to firebase

--- src/main.ts
+++ src/main.ts
@@ -11,21 +11,35 @@ import "@/assets/css/darkmode.css";
 
 import ToggleButton from "vue-js-toggle-button";
 
-import firebase from "firebase";
-import { Route } from 'vue-router';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import { Route } from "vue-router";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD9utBzDbUKegR2OIMYGTQETUg3m5AQ-sk",
-  authDomain: "junebug-754bc.firebaseapp.com",
-  databaseURL: "https://junebug-754bc.firebaseio.com",
-  projectId: "junebug-754bc",
-  storageBucket: "junebug-754bc.appspot.com",
-  messagingSenderId: "789403761474",
-  appId: "1:789403761474:web:a198df78ecfcafe74b66db"
+	apiKey: "AIzaSyD9utBzDbUKegR2OIMYGTQETUg3m5AQ-sk",
+	authDomain: "junebug-754bc.firebaseapp.com",
+	databaseURL: "https://junebug-754bc.firebaseio.com",
+	projectId: "junebug-754bc",
+	storageBucket: "junebug-754bc.appspot.com",
+	messagingSenderId: "789403761474",
+	appId: "1:789403761474:web:a198df78ecfcafe74b66db",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
+const ref = firebase.firestore().doc("tasks/XVGM06JLf2E7TrJKJSYO");
+ref.set({
+	first: {
+		title: "first assignment",
+	},
+	second: {
+		title: "second assignment",
+	},
+}).then(() => {
+	console.log("success!");
+	console.log(ref);
+});
 
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
@@ -34,16 +48,15 @@ Vue.use(ToggleButton);
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+	router,
+	store,
+	render: h => h(App),
 }).$mount("#app");
 
 router.afterEach((to: Route) => {
-  if (to.meta.tab) {
-    document.querySelector("#tabs")?.classList.remove("d-none");
-  }
-  else {
-    document.querySelector("#tabs")?.classList.add("d-none");
-  }
-})
+	if (to.meta.tab) {
+		document.querySelector("#tabs")?.classList.remove("d-none");
+	} else {
+		document.querySelector("#tabs")?.classList.add("d-none");
+	}
+});
